6db0d2f941f6f032f460998f4a07a89675e778b0
Boris Vashov <vashovway@gmail.com>
2022-01-22 07:23:06 +0300

    Interface for NodeRepository

---
+++ Source/MindKeeper.Api/Data/Repositories/Nodes/INodeRepository.cs
@@ -0,0 +1,15 @@
+﻿using MindKeeper.Api.Data.Entities;
+using MindKeeper.Api.Data.Repositories.Nodes.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MindKeeper.Api.Data.Repositories.Nodes
+{
+    public interface INodeRepository
+    {
+        public Task<Node> Get(long id);
+        public Task<Node> Get(string name);
+        public Task<List<Node>> GetAll(NodeFilter filter);
+        public Task<Node> Create(int userId, string name, string descritpion, int typeId, int parentId);
+    }
+}
---
+++ Source/MindKeeper.Api/Data/Repositories/Nodes/NodeRepository.cs
@@ -0,0 +1,32 @@
+﻿using MindKeeper.Api.Data.Entities;
+using MindKeeper.Api.Data.Repositories.Nodes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindKeeper.Api.Data.Repositories.Nodes
+{
+    public class NodeRepository : INodeRepository
+    {
+        public Task<Node> Create(int userId, string name, string descritpion, int typeId, int parentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Node> Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Node> Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Node>> GetAll(NodeFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
