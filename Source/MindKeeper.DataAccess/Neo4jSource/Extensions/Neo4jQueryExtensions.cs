1e040fb809e4793598e96e1e072f3dbf70687422
Boris Vashov <vashovway@gmail.com>
2022-02-03 08:39:22 +0300

    add extensions for Neo4j query result and query building

---
+++ Source/MindKeeper.DataAccess/Neo4jSource/Extensions/Neo4jQueryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace MindKeeper.DataAccess.Neo4jSource.Extensions
+{
+    public static class Neo4jQueryExtensions
+    {
+        public static string AsProperties<T>(this T value)
+        {
+            var type = value.GetType();
+            var builder = new StringBuilder();
+
+            var properties = type.GetProperties();
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var prop = properties[i];
+                var property = $"{prop.Name}: ${prop.Name}";
+                builder.Append(property);
+
+                if (i < properties.Length - 1)
+                    builder.Append(',');
+            }
+
+            return builder.ToString();
+        }
+    }
+}
