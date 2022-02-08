2779fd45ca2c7f8419ea28993fcdd15e3c7e6377
Boris Vashov <vashovway@gmail.com>
2022-02-08 14:13:33 +0300

    WIP: Statistics. Get achivements

--- Source/MindKeeper.DataAccess/Neo4jSource/Repositories/AchievementsRepository.cs
+++ Source/MindKeeper.DataAccess/Neo4jSource/Repositories/AchievementsRepository.cs
@@ -1,10 +1,10 @@
-﻿using MindKeeper.Domain.Entities;
+﻿using MindKeeper.DataAccess.Neo4jSource.Tokens;
+using MindKeeper.Domain.Entities;
 using MindKeeper.Domain.Interfaces;
 using Neo4j.Driver;
+using Neo4j.Driver.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MindKeeper.DataAccess.Neo4jSource.Repositories
@@ -18,9 +18,49 @@ namespace MindKeeper.DataAccess.Neo4jSource.Repositories
             _client = driver;
         }
 
-        public Task<List<Achievement>> GetAchievements(Guid? userId)
+        public async Task<List<Achievement>> GetAchievements(Guid? userId)
         {
-            throw new NotImplementedException();
+            string query = $@"
+                    MATCH (u:{Label.User})-[r:{Relationship.HAS_ACHIEVEMENT}->(a:{Label.Achievement})
+                    RETURN count(u) as count
+                    UNION ALL
+                    MATCH (i:{Label.Idea})
+                    RETURN count(i) as count
+                ";
+
+            object parameters;
+
+            if (userId.HasValue)
+            {
+                parameters = new
+                {
+                    UserId = userId
+                };
+
+                query = $@"
+                    MATCH (u:{Label.User} {{Id:$UserId}})-[r:{Relationship.HAS_ACHIEVEMENT}->(a:{Label.Achievement})
+                    RETURN a;
+                ";
+            }
+            else
+            {
+                parameters = new { };
+                query = $@"
+                    MATCH (a:{Label.Achievement})
+                    RETURN a;
+                ";
+            }
+
+            using var session = _client.AsyncSession();
+
+            var cursor = await session.RunAsync(query, parameters);
+            List<Achievement> results = await cursor.ToListAsync<Achievement>(r =>
+            {
+                var node = r["a"] as INode;
+                return node.ToObject<Achievement>();
+            });
+
+            return results;
         }
     }
 }
